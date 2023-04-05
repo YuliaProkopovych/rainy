962653c8be9cf2047ac4034d7a289aab28af4ea3
Yulia <schonebud@gmail.com>
2023-04-05 22:22:07 +0300

    Get forecast from meteorological server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use reqwest::header::USER_AGENT;
+use std::collections::HashMap;
+
 
 // This struct represents state
 struct AppState {
@@ -7,11 +10,28 @@ struct AppState {
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+  let response = get_forecast().await.unwrap();
+
+    HttpResponse::Ok().body(response)
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
+async fn get_forecast() -> Result<String, Box<dyn std::error::Error>> {
+  let client = reqwest::Client::new();
+  let response = client.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=34&lon=65") // <- Create request builder
+  .header(USER_AGENT, "https://github.com/YuliaProkopovych/weather-forecast")
+  .send()
+  .await
+  .expect("failed to get response")
+  .text()
+  .await
+  .expect("failed to get payload");
+  println!("{:#?}", response);
+  println!("Response: {:?}", "response");
+  Result::Ok(response)
+}
+
+#[post("/forecast")]
+async fn forecast(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
@@ -19,12 +39,16 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+// fastify.register(require('./routes/forecast'));
+// fastify.register(require('./routes/solar-forecast'));
+// fastify.register(require('./routes/nearby-locations'));
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
+            .service(forecast)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
