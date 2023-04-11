d263a6d3de2a0e88823e528bb1472d8e4362254d
Yulia <schonebud@gmail.com>
2023-04-11 17:46:54 +0300

    Implement solar-forecast endpoint

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,8 @@ use std::collections::HashMap;
 use serde_json;
 use serde_derive::{ Deserialize, Serialize };
 use std::env;
-
-// This struct represents state
-struct AppState {
-  coordinates: (f32, f32),
-}
+use chrono::prelude::*;
+use chrono::offset::LocalResult;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ForecastRecord {
@@ -48,8 +45,6 @@ async fn get_forecast(coordinates: &Coordinates) -> Result<Vec<ForecastRecord>, 
   let timeseries = &properties["timeseries"];
   let array = timeseries.as_array().expect("wowowow");
   let formatted_vec: Vec<ForecastRecord> = format_forecast((*array).clone());
-
-  //println!("{:#?}", formatted_vec[0].next_hour.as_ref().unwrap());
 
   Result::Ok(formatted_vec)
 }
@@ -133,11 +128,10 @@ struct ForecastInfo {
 
 #[post("/forecast")]
 async fn forecast(coordinates: web::Json<Coordinates>) -> impl Responder {
-  //println!("Status: {:#?}", coordinates);
   let coords = coordinates.into_inner();
   let forecast = get_forecast(&coords).await.expect("fdsodfsd");
   let timezone = get_timezones(&coords).await.expect("sdfsdfsd");
-  println!("Status: {:#?}", timezone);
+
   let response = ForecastInfo {
     forecast,
     coordinates: coords,
@@ -149,21 +143,101 @@ async fn forecast(coordinates: web::Json<Coordinates>) -> impl Responder {
   HttpResponse::Ok().body(res_json)
 }
 
+#[derive(Deserialize, Debug)]
+struct Info {
+    coordinates: String,
+    startDate: String,
+    endDate: String,
+}
+
+async fn get_sunrise(coords:&Coordinates, start_date: String, end_date: String, offset_string: String) -> Result<String, Box<dyn std::error::Error>> {
+
+  let start = start_date.clone() + " 00:00:00";
+  let end = end_date + " 00:00:00";
+
+  let start = Utc.datetime_from_str(&start, "%Y-%m-%d %H:%M:%S").unwrap();
+  let end = Utc.datetime_from_str(&end, "%Y-%m-%d %H:%M:%S").unwrap();
+
+  let diff = end - start;
+
+  let client = reqwest::Client::new();
+  let url = format!("https://api.met.no/weatherapi/sunrise/2.0/.json?lat={0}&lon={1}&date={2}&offset={3}&days={4}",
+    coords.lat, coords.lon, start_date, offset_string, diff.num_days());
+  let response = client.get(url)
+    .header(USER_AGENT, "https://github.com/YuliaProkopovych/weather-forecast")
+    .send()
+    .await?;
+
+    let res_json = response.json::<serde_json::Value>().await?;
+    let data = &res_json["location"];
+    let array = &data["time"];
+    println!("{:?}", data );
+
+
+  // const response = await request;
+  // const data = response.data.location.time;
+  // data.pop(); //????
+
+    Result::Ok(array.clone().to_string())
+
+}
+
+#[derive(Serialize, Debug)]
+struct SolarData {
+    coordinates: Coordinates,
+    solarData: String,
+    timezone: String,
+}
+
+#[get("/solar-forecast")]
+async fn solar_forecast(info: web::Query<Info>) -> impl Responder {
+  let parts: Vec<&str> = info.coordinates.split(",").collect();
+  let coords = Coordinates {
+    lat: parts[0].parse::<f64>().unwrap(),
+    lon: parts[1].parse::<f64>().unwrap(),
+  };
+
+  let timezone = get_timezones(&coords).await.expect("sdfsdfsd");
+  let offset = timezone.offset;
+  let mut offset_string = format!( "{0}:{1}", ((offset.trunc()).abs()),(60.0 * (offset.fract())));
+    if offset.abs() < 10.0 {
+      offset_string = format!("0{}", offset_string);
+    }
+    if offset.fract() == 0.0 {
+      offset_string = format!("{}0", offset_string);
+    }
+    if offset >= 0.0 {
+      offset_string = format!("+{}", offset_string);
+    } else {
+      offset_string = format!("-{}", offset_string);
+    }
+
+
+  let solar_data = get_sunrise(&coords, info.startDate.clone(), info.endDate.clone(), offset_string).await.unwrap();
+  println!("{:?}", solar_data );
+  let response = SolarData {
+    coordinates: coords,
+    timezone: timezone.timezoneId,
+    solarData: solar_data
+  };
+  let res_json = serde_json::to_string(&response).unwrap();
+
+  //   reply.send({ solarData, coordinates, timezone });
+
+  HttpResponse::Ok().body(res_json)
+}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-// fastify.register(require('./routes/forecast'));
-// fastify.register(require('./routes/solar-forecast'));
-// fastify.register(require('./routes/nearby-locations'));
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  env::set_var("RUST_BACKTRACE", "1");
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(forecast)
+            .service(solar_forecast)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
